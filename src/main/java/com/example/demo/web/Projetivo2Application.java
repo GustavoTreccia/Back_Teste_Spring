39b39e2829aad1886f988f682c30ecf752179bfd
GustavoTreccia <g.treccia@outlook.com>
2020-11-16 16:54:42 -0300

    Backup

---
+++ src/main/java/com/example/demo/web/Projetivo2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetivo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetivo2Application.class, args);
+	}
+
+}
